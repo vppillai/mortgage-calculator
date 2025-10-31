646af06b552f68dd9f1983bfb03b62e3d300dd52
Vysakh Pillai <vysakhpillai@gmail.com>
2025-10-30 18:14:28 -0700

    chore: update version information in version.js and version.json
    - Increment version to v1.0.0-23
    - Update commit hash and build time for the new version

--- public/version.js
+++ public/version.js
@@ -1,8 +1,8 @@
 export default {
-  "version": "v1.0.0-22-g09d78f5-dirty",
+  "version": "v1.0.0-23-gfc42517-dirty",
   "isRelease": false,
-  "commit": "09d78f54354ce87d1bc47cf65e988288147f5ec1",
-  "commitShort": "09d78f5",
-  "buildTime": "2025-10-31T01:13:18.655Z",
+  "commit": "fc42517f44b9f651393b049f3f7a8eb3a24ced21",
+  "commitShort": "fc42517",
+  "buildTime": "2025-10-31T01:13:48.142Z",
   "buildTimeLocal": "Oct 31, 2025, 01:13 AM UTC"
 };
