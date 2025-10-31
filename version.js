58045df7788e2270364bc5b8cfcdd210f55df0da
vppillai <vppillai@users.noreply.github.com>
2025-10-31 21:23:11 +0000

    deploy: 641f3de27e2f45bf58200b38c056f2aea7e05ae9

--- version.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "version": "5d1400f-dirty",
-  "isRelease": false,
-  "commit": "5d1400f3e550d7f075cf31582637846ac67e02e5",
-  "commitShort": "5d1400f",
-  "buildTime": "2025-10-31T15:08:07.892Z",
-  "buildTimeLocal": "Oct 31, 2025, 03:08 PM UTC"
-};
